d9506733268db25d5244430f3806ce88e2f59b9c
Veonisia <102913473+Veonisia@users.noreply.github.com>
2022-08-25 18:21:03 +0200

    Create BossBattle.lua

---
+++ BossBattle.lua
@@ -0,0 +1,24 @@
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local timerr = Instance.new("TextLabel")
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.Position = UDim2.new(0.461119741, 0, 0, 0)
+Frame.Size = UDim2.new(0, 100, 0, 30)
+
+UICorner.Parent = Frame
+
+timerr.Name = "timerr"
+timerr.Parent = Frame
+timerr.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+timerr.BackgroundTransparency = 1.000
+timerr.Size = UDim2.new(0, 100, 0, 30)
+timerr.Font = Enum.Font.FredokaOne
+timerr.TextColor3 = Color3.fromRGB(0, 0, 0)
+timerr.TextScaled = true
+timerr.TextSize = 14.000
+timerr.TextWrapped = true
